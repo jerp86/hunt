78f9d8ac922cc7c2e3a1ff6c2b3a4d569c5541a6
jerp86 <jerp4@hotmail.com>
2019-10-12 22:03:15 -0300

    Configurando navegação

--- App.js
+++ App.js
@@ -1,30 +1,6 @@
 import React from "react";
-import { StyleSheet, Text, View } from "react-native";
+import App from "./src";
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text style={styles.welcome}>#Teste</Text>
-      <View style={styles.box} />
-    </View>
-  );
-}
+const AppFunc = () => <App />;
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: "center",
-    alignItems: "center",
-    backgroundColor: "#f5fcff"
-  },
-  welcome: {
-    fontSize: 40,
-    textAlign: "center",
-    margin: 10
-  },
-  box: {
-    width: 60,
-    height: 70,
-    backgroundColor: "#F00"
-  }
-});
+export default AppFunc;
